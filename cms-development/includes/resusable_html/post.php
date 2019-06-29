d37a5c8d932f5a6ad2c850c9c559cfedec1125d0
ndth95 <52303280+ndth95@users.noreply.github.com>
2019-06-28 20:56:52 -0400

    Add files via upload

---
+++ cms-development/includes/resusable_html/post.php
@@ -0,0 +1,53 @@
+<?php 
+    // Extract all the data from the database of post
+    $postConnection = new Database_Connection($hostname, $username, $userpass, $tablename);
+    $sqlCmd = "SELECT * FROM Post";
+    $postData = $postConnection->getData($sqlCmd);
+    
+    // Print out all the information from post
+    $counter = 0;
+    for ($counter; $counter < count($postData); ++$counter){
+        $post_title = $postData[$counter]['Post_Title'];
+        $post_author = $postData[$counter]['Post_Author'];
+        $post_date = $postData[$counter]['Post_Date'];
+        $post_image = $postData[$counter]['Post_Image'];
+        $post_content = $postData[$counter]['Post_Content']; ?>
+       	
+<h1 class="page-header">
+    Page Heading
+    <small>Secondary Text</small>
+</h1>
+       	
+<h2>
+	<a href="#"><?php echo $post_title?></a>
+</h2>
+
+<p class="lead">
+    by <a href="index.php"><?php echo $post_author?></a>
+</p>
+
+<p><span class="glyphicon glyphicon-time"></span> Posted on <?php echo $post_date?></p>
+
+<hr>
+
+<img class="img-responsive" src="https://www.intofilm.org/intofilm-production/scaledcropped/970x546https%3A/s3-eu-west-1.amazonaws.com/images.cdn.filmclub.org/film__2838-howl-s-moving-castle--hi_res-90ac09c2.jpg/film__2838-howl-s-moving-castle--hi_res-90ac09c2.jpg" alt="">
+
+<hr>
+
+<p><?php echo $post_content?></p>
+
+<a class="btn btn-primary" href="#">Read More <span class="glyphicon glyphicon-chevron-right"></span></a>
+
+<hr>
+        
+<?php
+    }
+?>
+
+
+
+
+
+
+
+
